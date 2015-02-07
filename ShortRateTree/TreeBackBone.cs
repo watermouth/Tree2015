5bd1db0c0eecb65f13d750cc4ec0bd4c0a1b5806
sm <sm@dummy>
2015-02-07 16:02:32 +0900

    Defined TreeBackBone and TreeNode

---
+++ ShortRateTree/TreeBackBone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortRateTree
+{
+    /// <summary>
+    /// 32Byte
+    /// </summary>
+    struct TreeBackBone
+    {
+        double V;
+        double dx;
+        double dt;
+        double alpha;
+    }
+}
---
+++ ShortRateTree/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortRateTree
+{
+    /// <summary>
+    /// 44Byte
+    /// </summary>
+    struct TreeNode
+    {
+        short j;
+        short k;
+        double pu;
+        double pm;
+        double pd;
+        double Q;
+        double r;
+    }
+}
