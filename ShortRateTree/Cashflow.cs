ff3595bc69b2dfd9ebe8a7320fc551eff8533a22
sm <sm@dummy>
2015-02-14 22:02:08 +0900

    CashflowとTimeIntervalを追加

---
+++ ShortRateTree/Cashflow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortRateTree
+{
+    public class Cashflow
+    {
+        public DateTime ExerciseDate;
+        public DateTime ResetDate;
+        public DateTime SettlementDate;
+        public double SwapRate;
+    }
+}
---
+++ ShortRateTree/TimeInterval.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ShortRateTree
+{
+    /// <summary>
+    /// Bermudan Swaption 評価用のTime Interval
+    /// </summary>
+    public class TimeInterval
+    {
+        /// <summary>
+        /// この区間内のツリー分割時点
+        /// </summary>
+        public double[] TreeTimes;
+        /// <summary>
+        /// この区間の最大時点に対するツリー時点のindex
+        /// </summary>
+        public int MaxTreeTimeIndex;
+        /// <summary>
+        /// この区間の右端の時点が、権利行使時点か 
+        /// </summary>
+        public bool IsExerciseDate;
+        /// <summary>
+        /// この区間の左端の時点が、割引債の満期時点であるか 
+        /// </summary>
+        public bool IsDiscountBondPriceMaturity;
+
+        public void DivideTimeInterval(DateTime baseDate, DateTime resetDate, DateTime settleDate, double divideIntervalDays)
+        {
+            Debug.Assert(DateTime.Compare(resetDate, settleDate) < 0);
+            int d = (int)Math.Round((settleDate - resetDate).TotalDays / divideIntervalDays, MidpointRounding.AwayFromZero);
+            TreeTimes = new double[d+1];
+            for (int i = 0; i <= d; ++i)
+            {
+                TreeTimes[i] = (resetDate.AddDays(i * divideIntervalDays) - baseDate).TotalDays / 365D;
+            }
+        }
+    }
+}
